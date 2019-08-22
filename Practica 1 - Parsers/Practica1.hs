4d4e73d1275deba01d9b8e484fd265e348b59641
gpaoloni <paolonigianfranco@gmail.com>
2019-08-21 23:34:09 -0300

    Resueltos 1.6 y 1.7

--- Practica 1 - Parsers/Practica1.hs
+++ Practica 1 - Parsers/Practica1.hs
@@ -101,6 +101,61 @@ hetero      =  do char '['
                   return x
 
 -- 6) 
+data Basetype = DInt | DChar | DFloat
+                deriving Show
+type Hasktype = [Basetype]
+
+intbase     :: Parser Basetype
+intbase     =  do string "Int"
+                  return DInt
+
+charbase    :: Parser Basetype
+charbase    =  do string "Char"
+                  return DChar
+
+floatbase   :: Parser Basetype
+floatbase   =  do string "Float"
+                  return DFloat
+
+parsebase   :: Parser Basetype
+parsebase   =  do intbase
+                <|> (do charbase
+                      <|> floatbase)
+
+typeparser  :: Parser Hasktype                                          -- para eliminar espacios blancos
+typeparser  =  do x <- sepBy (token parsebase) (string "->")            -- x <- sepBy parsebase (string " -> ")
+                  return x
+
+-- 7) 
+data Haskelltype = DDInt | DDChar | Fun Haskelltype Haskelltype
+                   deriving Show
+
+intbase'    :: Parser Haskelltype
+intbase'    =  do string "Int"
+                  return DDInt
+
+charbase'   :: Parser Haskelltype
+charbase'   =  do string "Char"
+                  return DDChar
+
+parsebase'  :: Parser Haskelltype
+parsebase'  =  do intbase'
+                <|> charbase'
+
+typepar     :: Parser Haskelltype           -- para cantidad arbitraria de espacios (en cualquier lugar):
+typepar     =  do t1 <- parsebase'          -- t1 <- token parsebase'
+                  t2 <- typepar'
+                  return (Fun t1 t2)
+
+typepar'    :: Parser Haskelltype
+typepar'    =  do string " -> "             -- string "->"
+                  t2 <- parsebase'          -- t2 <- token parsebase'
+                  (do t3  <- typepar'
+                      return (Fun t2 t3)
+                    <|> return t2)
+                  
+                  
+
 
 
 
